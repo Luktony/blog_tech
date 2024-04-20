e78e037eb4b6f01f3db6125a5a7b54cc5fe50664
Lucas <luktony7@yahoo.com.br>
2024-04-20 12:07:23 -0300

    "Adding functionality to edit a post in the controller"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,18 @@ def new
     end
     
   end
-
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+    
+  end
  private
   def post_params
     params.require(:post).permit(:title, :body)
